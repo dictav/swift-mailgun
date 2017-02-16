f9ce4286cc8806bdcac549c907d0bbc88afc21c2
Shintaro Abe <dictav@gmail.com>
2017-02-16 17:47:08 +0900

    Move test configuration to MailgunTests.swift

--- Tests/MailgunTests/DomainTests.swift
+++ Tests/MailgunTests/DomainTests.swift
@@ -3,12 +3,6 @@ import XCTest
 
 extension MailgunTests {
     func testDomainList() {
-        guard let key = ProcessInfo.processInfo.environment["MAILGUN_API_KEY"] else {
-            XCTAssert(false, "PLEASE SET $MAILGUN_API_KEY")
-            return
-        }
-
-        Mailgun(key: key)
         let result = Domain.list()
         XCTAssertNil(result.error)
     }
--- Tests/MailgunTests/MailgunTests.swift
+++ Tests/MailgunTests/MailgunTests.swift
@@ -2,6 +2,15 @@ import XCTest
 @testable import Mailgun
 
 class MailgunTests: XCTestCase {
+    override func setUp() {
+        guard let key = ProcessInfo.processInfo.environment["MAILGUN_API_KEY"] else {
+            XCTAssert(false, "PLEASE SET $MAILGUN_API_KEY")
+            return
+        }
+
+        Mailgun(key: key)
+    }
+
     static var allTests : [(String, (MailgunTests) -> () throws -> Void)] {
         return [
             ("testResult", testResult),

cfcd69af244aadc3d510cc3c39a218e5968f66c1
Shintaro Abe <dictav@gmail.com>
2017-02-24 20:30:59 +0900

    Add dependency `antitypical/Result`

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "Mailgun"
+    name: "Mailgun",
+    dependencies: [
+        .Package(url: "https://github.com/antitypical/Result.git", majorVersion:3)
+    ]
 )
